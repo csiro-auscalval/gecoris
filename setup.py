6447c81a78106cf90d497923b17dfe8e4217b7cf
Rowan Winsemius <rowanwins@yahoo.com.au>
2023-02-24 15:18:57 +1100

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gecoris',
+    version='0.0.1',
+    description='',
+    entry_points={
+    },
+    url='https://github.com/csiro-auscalval/gecoris',
+    setup_requires=[],
+    packages=['geocoris']
+)
